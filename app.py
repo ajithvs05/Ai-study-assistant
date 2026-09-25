3ad69e035b2ce1d4d1e870a089771881c2c7c776
Ajith vs <ajithvs904@gmail.com>
2026-09-25 20:33:22 +0530

    Add Quiz Mode and update menu options

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ while True:
     print("\n1. Study Planner")
     print("2. Study Notes")
     print("3. AI Questions")
-    print("4. Study Tips")
-    print("5. Exit")
+    print("4. Quiz Mode")
+    print("5. Study Tips")
+    print("6. Exit")
 
     choice = input("\nChoose an option: ")
 
@@ -44,13 +45,55 @@ while True:
             print("AI: I don't have an answer for that yet.")
 
     elif choice == "4":
+        questions = [
+            ("What does CPU stand for?",
+             ["A. Central Processing Unit",
+              "B. Computer Personal Unit",
+              "C. Central Program Utility",
+              "D. Control Processing User"],
+             "A"),
+
+            ("Which language is used in this project?",
+             ["A. Java",
+              "B. Python",
+              "C. C++",
+              "D. HTML"],
+             "B"),
+
+            ("What does AI stand for?",
+             ["A. Automated Internet",
+              "B. Artificial Intelligence",
+              "C. Advanced Information",
+              "D. Automatic Interface"],
+             "B")
+        ]
+
+        score = 0
+
+        for question, options, answer in questions:
+            print("\n" + question)
+
+            for option in options:
+                print(option)
+
+            user_answer = input("Your answer: ").upper()
+
+            if user_answer == answer:
+                print("✅ Correct!")
+                score += 1
+            else:
+                print("❌ Wrong!")
+
+        print("\nYour Score:", score, "/", len(questions))
+
+    elif choice == "5":
         print("\n💡 Study Tips")
         print("• Study for 45 minutes.")
         print("• Take a short break.")
         print("• Practice previous questions.")
         print("• Revise regularly.")
 
-    elif choice == "5":
+    elif choice == "6":
         print("\nThank you for using AI Study Assistant! 👋")
         break
 
